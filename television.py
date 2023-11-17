09853a29f258145d5f1cff3028265b60db70f617
RandomeMar <151096223+RandomeMar@users.noreply.github.com>
2023-11-16 20:02:35 -0600

    Added docstrings and type hinting to television.py file

--- television.py
+++ television.py
@@ -5,18 +5,27 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self):
-        self.__status = False
-        self.__muted = False
-        self.__volume = self.MIN_VOLUME
-        self.__channel = self.MIN_CHANNEL
+        """
+        Method to set default values of television object
+        """
+        self.__status: bool = False
+        self.__muted: bool = False
+        self.__volume: int = self.MIN_VOLUME
+        self.__channel: int = self.MIN_CHANNEL
 
     def power(self):
+        """
+        Method to flip value of status variable
+        """
         if not self.__status:
             self.__status = True
         else:
             self.__status = False
 
     def mute(self):
+        """
+        Method to flip value of muted variable
+        """
         if not self.__status:
             return
         if not self.__muted:
@@ -25,6 +34,9 @@ class Television:
             self.__muted = False
 
     def channel_up(self):
+        """
+        Method to raise channel level and cycle from the maximum channel to the minimum
+        """
         if not self.__status:
             return
         if self.__channel == self.MAX_CHANNEL:
@@ -33,6 +45,9 @@ class Television:
             self.__channel += 1
 
     def channel_down(self):
+        """
+        Method to lower channel level and cycle from the minimum channel to the maximum
+        """
         if not self.__status:
             return
         if self.__channel == self.MIN_CHANNEL:
@@ -41,6 +56,9 @@ class Television:
             self.__channel -= 1
 
     def volume_up(self):
+        """
+        Method to increase volume level unless volume is maxed out
+        """
         if not self.__status:
             return
         if self.__muted:
@@ -50,6 +68,9 @@ class Television:
         self.__volume += 1
 
     def volume_down(self):
+        """
+        Method to decrease volume level unless volume is at its minimum
+        """
         if not self.__status:
             return
         if self.__muted:
@@ -58,8 +79,11 @@ class Television:
             return
         self.__volume -= 1
 
-    def __str__(self):
-        out_vol = self.__volume
+    def __str__(self) -> str:
+        """
+        Method that returns power status, channel level, and volume level when the object is printed
+        """
+        out_vol: int = self.__volume
         if self.__muted:
             out_vol = self.MIN_VOLUME
-        return f'Power = {self.__status}, Channel; = {self.__channel}, Volume = {out_vol}'
+        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {out_vol}'
